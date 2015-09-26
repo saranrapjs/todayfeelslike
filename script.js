579021a39be47e023549969110a3976edde8fa51
Jeff Sisson <jeffsisson@gmail.com>
2015-09-26 14:15:54 -0400

    deeper pie chart stuff

--- script.js
+++ script.js
@@ -1,51 +1,122 @@
-var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
-
-var x = d3.scale.linear()
-    .range([0, width]);
-
-
-
-
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d; });
-
-var svg;
-
 function update_counts(data) {
+	var width = parseInt(d3.select('.chart').style('width'), 10),
+	    height = Math.min(400, width * 0.4),
+	    radius = Math.min(width, height) / 2;
+
+	var x = d3.scale.linear()
+	    .range([0, width]);
+
+	var arc = d3.svg.arc()
+	    .outerRadius(radius * 0.8)
+	    .innerRadius(0);
+
+	var outerArc = d3.svg.arc()
+		.innerRadius(radius * 0.9)
+		.outerRadius(radius * 0.9);
+
+	var pie = d3.layout.pie()
+	    .sort(null)
+	    .value(function(d) { return d.value; });
+
+	var total = d3.sum(data, function(d) { 
+		return d; 
+	});
+
+	var max = 0,
+		maxIndex = 0;
+
+	data.forEach(function(d, n) {
+		if (d > max) {
+			max = d;
+			maxIndex = n;
+		}
+	});
+
+	data = data.map(function(d, n) {
+		var pct = Math.round((d / total) * 100);
+		return {
+			value: d,
+			is_max: (n === maxIndex),
+			pct: Math.round((d / total) * 100)
+		};
+	});
+
+	var label_data = data.filter(function(d) {
+		return d.pct > 1;
+	});
+
 	var svg = d3.select(".chart")
 	    .attr("width", width)
 	    .attr("height", height)
 	  .append("g")
 	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-	var g = svg.selectAll(".arc")
-	      .data(pie(data))
-	    .enter().append("g")
-	      .attr("class", function(d,n) {
-	      	return "arc " + moment().day(n).format('dddd').toLowerCase();
-	      });
+	svg.append("g")
+		.attr("class", "slices");
+	svg.append("g")
+		.attr("class", "labels");
+	svg.append("g")
+		.attr("class", "lines");
 
-	  g.append("path")
-	      .attr("d", arc)
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data))
+		.enter()
+			.append("g")
+			.attr("class", function(d,n) {
+				var is_max = d.is_max ? ' is-max' : '';
+				return "slice " + moment().day(n).format('dddd').toLowerCase() + is_max;
+			});
 
-	  g.append("text")
-	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-	      .attr("dy", ".35em")
-	      .style("text-anchor", "middle")
-	      .text(function(d,n) { 
-	      	return moment().day(n).format('dddd');
-	      });
+	slice.append("path")
+		.attr("d", arc)
+
+	slice
+		.append("text")
+		.attr('class', 'numbers-label')
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy", ".35em")
+		.style("text-anchor", "middle")
+		.text(function(d,n) {
+			return Math.abs(d.startAngle - d.endAngle) > 0.1 ? d.value : '';
+		});
+
+	var text = svg.select(".labels").selectAll("text")
+			.data(pie(label_data));
+
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.text(function(d, n) {
+			return moment().day(n).format('dddd') + ' (' + d.data.pct + '%)';
+		})
+		.attr('text-anchor', function(d) {
+			return midAngle(d) < Math.PI ? "start":"end";
+		})
+		.attr('transform', function(d) {
+			var pos = outerArc.centroid(d);
+			pos[0] = radius * (midAngle(d) < Math.PI ? 1 : -1);
+			return "translate("+ pos +")";
+		})
+
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(label_data));
+
+	polyline.enter()
+		.append("polyline")
+		.attr('class', 'lines')
+		.attr('points', function(d) {
+			var pos = outerArc.centroid(d);
+			pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+			var innerpos = arc.centroid(d).map(function(pos) {
+				return pos * 1.8;
+			});
+			return [innerpos, outerArc.centroid(d), pos];
+		})
 }
 
 var next_time,
@@ -53,11 +124,6 @@ var next_time,
 	now = moment();
 
 window.addEventListener('load', function() {
-	svg = d3.select(".chart")
-		.attr("width", width)
-		.attr("height", height)
-		.append("g")
-		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 	document.querySelector('.today').textContent = moment().format('dddd');
 	update_counts(JSON.parse(document.body.getAttribute('data-chart')));
@@ -66,6 +132,10 @@ window.addEventListener('load', function() {
 
 	if (document.body.dataset && document.body.dataset.nextrefresh) {
 		next_time = moment(new Date(document.body.dataset.nextrefresh));
+
+		if (next_time.diff(now) < -5000) {
+			return;
+		}
 
 		setTimeout(function() {
 			window.location.reload();
